82eb6c8f28e731b35d020ffdf13af94d844b8d92
FahimX <sakibfahim270@gmail.com>
2025-05-15 04:15:32 +0800

    database-setup

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -9,6 +11,31 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Coffee server is getting hotter");
 });
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.glnanjr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0;`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 app.listen(port, () => {
   console.log(`Coffee server is running on port: ${port}`);
 });
