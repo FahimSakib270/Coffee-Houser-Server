b212a15912c37dd39c50873875942f1045d55aaa
FahimX <sakibfahim270@gmail.com>
2025-05-15 07:08:44 +0800

    handling data delete

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -36,6 +36,13 @@ async function run() {
       const newCoffee = req.body;
       console.log("Received new coffee data:", newCoffee);
       const results = await coffeeCollection.insertOne(newCoffee);
+      res.send(results);
+    });
+
+    app.delete("/coffee/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const results = await coffeeCollection.deleteOne(query);
       res.send(results);
     });
 
